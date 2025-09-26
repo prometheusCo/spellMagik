3e2b03aa9bdd7cf5738a960a06a31497268e9ab6
prometheusCo <143731957+prometheusCo@users.noreply.github.com>
2025-09-26 18:01:09 +0200

    TS

--- main.js
+++ main.js
@@ -6,60 +6,57 @@ class coreFunctions {
     vowelsClosed = ['i', 'u'];
     vowels = [...this.vowelsOpened, ...this.vowelsClosed];
 
-    consonantsS = ['p', 't', 'k', 'b', 'd', 'g'];
-    consonantsF = ['f', 's', 'j', 'z', 'c', 'y', 'll'];
-    consonantsL = ['l', 'r'];
+    plosives = ["p", "t", "k", "b", "d", "g"];
+    fricatives = ["f", "s", "j", "z"];
+    affricates = ["ch"];
+    nasals = ["m", "n", "ñ"];
+    laterals = ["l", "ll"];
+    approximants = ["b", "x"];
+    vibrants = ["r", "rr"];
+
 
     accents = ['á', 'é', 'í', 'ó', 'ú'];
 
     diphthongs = [
         "ia", "ie", "io", 'uei',
-        "ua", "ue", "uo",
+        "ua", "ue", "uo", "ió",
         "ai", "ei", "oi",
         "au", "eu", "ou",
-        "iu", "ui", "ai"
+        "iu", "ui", "ai",
+        "üi", "üe", "üa", "üe",
+        "üé", "eu", "uí"
     ];
 
+    // Valid two-consonant ONSET clusters by TYPE 
+    validOnset2ByType = [
+        "PCLC", // pl, bl, cl, gl
+        "PCVC", // pr, br, tr, dr, cr, gr
+        "FCLC", // fl
+        "FCVC", // fr
+    ];
+
+    // Valid two-consonant CODA clusters by TYPE 
+    validCoda2ByType = [];
+
+    valid2CSounds = [...this.validOnset2ByType, ...this.validCoda2ByType];
     diphthongsExceptions = ["uí", "üí"]
 
-    syllablesThatStartsWithVowel = [
-        //A
-        'an', 'amb', 'amp', 'al', 'arr', 'ar', 'ads',
-        //E
-        'es', 'en', 'emp', 'en', 'err', 'er',
-        //I
-        'ins', 'im', 'in', 'or',
-        //O
-        'obs', 'os', 'on',
-        //U
-        'un'
+    forbidenEnds = ["c", "k"]
+    forbidenEndsExc = [
+        "pec", "truc", "trac", "vic", "dic", "fec", "fac", "ac",
+        "obs", "dac", "cons"
+
     ];
-
-    validSyllablesEst = ['CSCL', 'CFCL', 'CSVCL', 'CFC',
-
-        'dr', 'dl', 'ph', 'ps', 'rr', 'bl', 'ch', 'll', 'rr', 'br', 'tl', 'tr',
-        "pl", "cl", "fl", "gl",
-        "pr", "br", "cr", "fr", "gr"
-    ]
-
-    validJoints = ["bs", "ns", "nd", "rs", "ll", "si"];
-
-    invalidEnds = ["ch", "ll", "im"]
 
     constructor() { };
 
+    //
+    //Simple one liners helpers
     clean = s => s.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "");
-
     isCapitalized = word => /^[A-ZÁÉÍÓÚÑÜ]/.test(word);
-
     capitalize = s => s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
-
+    hasAccent(vowel) { return this.accents.includes(vowel); }
     pos = n => n < 0 ? n * (-1) : n;
-
-    insertAfterIndex(arr, index, value) {
-        arr.splice(index + 1, 0, value);   // mutates arr
-        return arr;
-    }
 
     //
     // Given a word, returns it's structure (VOWEL + CONSONANT + VOWEL...)
@@ -94,23 +91,20 @@ class coreFunctions {
     }
 
     //
-    // Gives true if structure given is a valid spanish syllable est
-    isValidSyllablesEst(syllablesEstAsArray, syllabesAsArray) {
+    // Move chars backwards/forward bettwen array's positions
+    moveAround(arr, pos, char, direction) {
 
-        return [...syllablesEstAsArray].some((est, index) => {
+        const to = direction === "right" ? pos + 1 : pos - 1;
+        const cutIndex = direction === "right" ? -1 : 1;
 
-            if (this.validSyllablesEst.includes(est) || this.validSyllablesEst.includes(syllabesAsArray[index]))
-                return true;
+        // cut char from current slot
+        arr[pos] = direction === "right" ? arr[pos].slice(0, cutIndex) : arr[pos].slice(cutIndex);
 
-            if (this.validSyllablesEst.includes(est.split("V")[0]))
-                return true;
+        // glue char to neighbor
+        arr[to] = direction === "right" ? char + arr[to] : arr[to] + char;
 
-            return false;
-        })
+        return arr;
     }
-    //
-    //
-    hasAccent(vowel) { return this.accents.includes(vowel); }
 
 }
 
@@ -125,18 +119,43 @@ class coreFunctionsExt extends coreFunctions {
     getEstExt(word, returnType = "array") {
 
         let wordAsArray = word.split("");
-        return super.getEst(word, returnType).map((est, index) => {
+        let r = super.getEst(word, returnType).map((est, index) => {
 
             if (est === "V") return "V";
 
-            if (this.consonantsS.includes(wordAsArray[index])) return "CS";
-            if (this.consonantsF.includes(wordAsArray[index])) return "CF";
-            if (this.consonantsL.includes(wordAsArray[index])) return "CL";
+            if (wordAsArray[index] === "c") {
+                if (wordAsArray[index + 1] === "e" || wordAsArray[index + 1] === "i") return "FC";
+                return "PC";
+            }
+
+            if (this.plosives.includes(wordAsArray[index])) return "PC";
+            if (this.fricatives.includes(wordAsArray[index])) return "FC";
+            if (this.affricates.includes(wordAsArray[index])) return "CA";
+            if (this.nasals.includes(wordAsArray[index])) return "NC";
+            if (this.laterals.includes(wordAsArray[index])) return "LC";
+            if (this.approximants.includes(wordAsArray[index])) return "AC";
+            if (this.vibrants.includes(wordAsArray[index])) return "VC";
 
             return "C";
-
         });
 
+        if (returnType !== "array") return r.join("");
+
+        return r;
+    }
+
+    //
+    //
+    isF2Valid(word) {
+
+        let f2 = word.slice(0, 2); let f3 = word.slice(0, 3); let est = this.getEstExt(f2, "str");
+
+        if (f2 === "cc" || word === "ch" || word === "rr" || word === "ll")
+            return false;
+        if (f2 === "rr" || f2 === "ll" || est === "CV" || f2 == "ch" || f2 === "ps" || f3 === "ciu" || f3 === "cie")
+            return true;
+
+        return this.valid2CSounds.includes(est);
     }
 }
 
@@ -146,14 +165,13 @@ class magikESpeller extends coreFunctionsExt {
 
 
     constructor() { super() }
+
     //  
     //Heuristic syllables spliter
     splitInSyllables(word) {
 
-        const cap = this.isCapitalized(word) ? true : false;
-        const isupc = cap && (word.charAt(word.length - 1).toLowerCase() !== word.charAt(word.length - 1)) ? true : false;
-        word = word.toLowerCase();
         console.time("miScript");
+        word = word.toLowerCase();
 
         let syllables = []; // syllabes array, each position is a syllable
         let syllablesTmp = ""; // tmp array used to store consonants bettwen found vowels
@@ -169,80 +187,49 @@ class magikESpeller extends coreFunctionsExt {
         // this checks if we have reached the end of the word and push wht's left in tmp to sayllables 
         const emptyPush = (_pointerCalc) => { if (_pointerCalc === wordAsArray.length - 1) { tmpPush("") }; }
 
-        // this fixes some problems with last chars 
-        const lastCharCheck = (_syllables) => {
+        //Check if latest push to sylables makes any sense and in case it doesn't, it fixes
+        const rulesApply = (syllabes) => {
 
-            if (this.getEst(_syllables.at(-1)) !== "C")
-                return syllables;
+            const lastS = syllabes.at(-1);
+            const lasSEst = this.getEst(lastS);
 
-            _syllables[_syllables.length - 2] = _syllables.at(-2) + _syllables.at(-1);
-            _syllables.pop();
-            return _syllables;
-        }
-        // This fixes wrong syllables that are made of one vowel, by moving letters around it does 2 loops but you
-        // can make it to loop N times just to be 100% sure
-        const postProcessing = (syllables) => {
+            const lastLastS = syllabes.at(-2) ?? false;
 
-            syllables[syllables.length - 1] = syllables.at(-1).replaceAll("undefined", "");
-            let r = lastCharCheck(syllables).filter((s) => s !== "");
+            if (!lastLastS) return syllabes;
 
-            console.log(r);
-            for (let x = 0; x < 2; x++) {
+            const lastLetterLastlastS = lastLastS[lastLastS.length - 1];
+            const lastLastEst = this.getEst(lastLastS) ?? false;
+            const firstLastS = lastS.slice(0, 1);
+            const firstLetLastEst = this.getEst(firstLastS)
 
-                r.forEach((syllable, index) => {
+            let conjuntion = lastLastS + lastS;
 
-                    let prevSyllable = r[index - 1] ?? false;
-                    let nextSyllable = r[index + 1] ?? false;
+            //Sanity check
+            if (conjuntion.length > 3) conjuntion = conjuntion.slice(-3);
 
-                    if (!prevSyllable)
-                        return;
+            // No syllable can be made of a consonant only
+            // If last letter from  prev syllable and first from current form a diphthong
+            // We join them together
+            if (lasSEst === "C" || (firstLetLastEst !== "C" && !!this.reverseSearch(conjuntion, this.diphthongs, true)))
+                this.moveAround(syllabes, syllabes.length - 1, lastS, "left");
 
-                    let prevLetter = prevSyllable.slice(prevSyllable.length - 1) ?? "";
-                    let prevSyllableEst = this.getEst(prevLetter);
-                    let syllabeEst = this.getEst(syllable);
-                    let prevPrevLetter = prevSyllable.slice(prevSyllable.length - 2) ?? "";
-                    let cutIndex = 1;
-                    let isValidEst = this.isValidSyllablesEst([this.getEstExt(syllable).join("")], [syllable]);
-                    let isValidEstVar1 = this.isValidSyllablesEst([this.getEstExt(syllable.slice(1)).join("")], [syllable]);
-                    let currentLetter = syllable.slice(0, 1);
+            //We check if there's a invalid 2 consonants syllable and fix it
+            if (lasSEst.slice(0, 2) === "CC" && !this.isF2Valid(lastS)) {
 
-
-                    if (!isValidEst && isValidEstVar1 ||
-                        (!!this.reverseSearch(prevLetter + syllable.slice(0, 1), this.validJoints) && !isValidEst)) {
-
-                        if (syllable === "ión") {
-
-                            r[index] = prevLetter + syllable;
-                            r[index - 1] = r[index - 1].slice(0, r[index - 1].length - 1);
-                            return;
-                        }
-
-
-                        r[index] = syllable.slice(1);
-                        r[index - 1] = r[index - 1] + currentLetter;
-                        return;
-                    }
-
-                    if (!this.diphthongs.includes(syllable) && prevSyllableEst + syllabeEst !== "CV" &&
-                        prevSyllableEst + syllabeEst !== "CVC" && (prevLetter + syllable.slice(0, 1) !== "rr"))
-                        return;
-
-                    if (prevPrevLetter === "rr") { prevLetter = "rr"; cutIndex = 2; }
-                    if (prevPrevLetter === "ll") { prevLetter = "ll"; cutIndex = 2; }
-
-                    r[index] = prevLetter + syllable;
-                    r[index - 1] = prevSyllable.slice(0, prevSyllable.length - cutIndex);
-
-                })
+                if (lastS == "ch" || lastS == "ll") {
+                    syllabes[syllabes.length - 2] = syllabes[syllabes.length - 2] + lastS;
+                    syllabes.pop();
+                    return syllabes;
+                }
+                this.moveAround(syllabes, syllabes.length - 1, firstLastS, "left");
             }
 
-            r = r.filter((s) => s !== "");
+            //Fixing invalid syllables endings
+            if (this.forbidenEnds.includes(lastLetterLastlastS) && !this.forbidenEndsExc.includes(this.clean(lastLastS)))
+                this.moveAround(syllabes, syllabes.length - 2, lastLetterLastlastS, "right");
 
-            if (this.invalidEnds.includes(r.at(-1))) {
-                r[r.length - 2] = r[r.length - 2] + r.at(-1); r.pop();
-            }
-
-            return r;
+            //Sanity check
+            return syllabes.filter((s) => s !== "");
         }
 
         //
@@ -251,147 +238,26 @@ class magikESpeller extends coreFunctionsExt {
         // saves it to syllables array, adding what's stored in tmp before it
         // it has rules to handle dipthongs and syllables that starts in vowel and end in consonant
         //
+
         for (let index = 0; index < wordAsArray.length; index++) {
 
-            let prevType = wordAsEstArray[index - 1] ?? "NN",
-                currentLetter = wordAsArray[index],
-                currentType = wordAsEstArray[index],
-                nextLetter = wordAsArray[index + 1] ?? false,
-                prevLetter = wordAsArray[index - 1] ?? "",
-                nextNextLetter = wordAsArray[index + 2] ?? "",
-                pointerCalc = () => syllables.join("").length + syllablesTmp.length - 1;
-            // code above calcs current pointer
-            // the pointer is the last pos of what's already splited in syllables
+            const currentType = wordAsEstArray[index];
+            const currentLetter = wordAsArray[index];
+            const pointerCalc = () => syllables.join("").length + syllablesTmp.length - 1;
 
-            const leftoverCalc = length => (pointerCalc() + length) === (word.length);
-
-            // This prevents adding again already added letters to syllables array when the pointer is moved forward 
-            // after a dipthong or a syllable that starts in vowel is found
-
-            if (index <= pointerCalc())
-                continue;
-
-            // Early negative return for when the current letter is a consonant
             if (currentType === "C") {
 
-                //
-                // => Adding current consonant to tmp  so it can be prepended to next's vowel's syllable  when found
-                tmpAdd(currentLetter);
-                if (index === wordAsArray.length - 1) { tmpPush("") }
+                tmpAdd(currentLetter); emptyPush(pointerCalc());
+                syllables.length > 0 ? syllables = [...rulesApply(syllables)] : null;
                 continue;
             }
 
-            // Early negative return for when there's no next letter
-            // Bellow code is for checking dipthongs and  syllables starting in vowels...
-            if (!nextLetter) { tmpPush(currentLetter); continue; }
-
-            //
-            //Here we form posible candidates for dipthongs and  syllables that starts...
-            //
-            let probJointSyllable2 = currentLetter + nextLetter; // in, ar, or, al type of syllables
-            let probJointSyllable3 = currentLetter + nextLetter + nextNextLetter; // ins, amp, amb... type of syllables
-            //
-            //here we store the results for  reverse searches, if nothing found var is false
-            let reverseSearchResult2 = this.reverseSearch((probJointSyllable2), this.syllablesThatStartsWithVowel, true);
-            let reverseSearchResult3 = this.reverseSearch((probJointSyllable3), this.syllablesThatStartsWithVowel, true);
-
-            // global result for reverse searches: if a reverse search with 3 letter is found we take that as the
-            // global result, otherwise we take the reverseSearchResult for 2 letters (something or false)
-            let reverseResult = !reverseSearchResult3 ? reverseSearchResult2 : reverseSearchResult3;
-
-            // Same as above but with the types of dipthongs
-            let probJointSyllable = this.diphthongs.includes(this.clean(probJointSyllable3)) ? probJointSyllable3 :
-                (this.diphthongs.includes(this.clean(probJointSyllable2)) ? probJointSyllable2 : false);
-
-            //
-            // === > Early negative return for cases in wich no dipthong or syllable that starts in vowel is found
-            // in those cases we added this vowel to the syllables array prepending current tmp data and skip 
-            //
-            if ((!probJointSyllable && !reverseResult)) {
-
-                if (nextLetter === nextNextLetter && nextLetter !== "l" && nextLetter !== "r")
-                    currentLetter += nextNextLetter;
-
-                if (this.getEst(nextLetter) === "C" && this.getEst(nextNextLetter) === "C" &&
-                    this.getEst(prevLetter) !== "V" && nextLetter !== nextNextLetter &&
-                    !this.validSyllablesEst.includes(nextLetter + nextNextLetter))
-                    currentLetter += nextLetter;
-
-                tmpPush(currentLetter);
-                continue;
-            }
-
-            // this is kind of tricky: is used to know wether we have reach the probable end
-            // in case of forming a syllable that starts in vowel.
-            let leftoverPointer = leftoverCalc(reverseResult.length);
-
-            //
-            // === > Pushing the syllable that starts in vowel to global result array and skiping  the rest
-            //
-            if (reverseResult !== false && !probJointSyllable) {
-
-                if (leftoverPointer)
-                    reverseResult = reverseResult.slice(0, reverseResult.length - 1)
-
-                if (reverseResult[0] !== currentLetter)
-                    reverseResult = currentLetter + reverseResult;
-
-                if ((this.getEst(reverseResult) === "VVC") && !this.reverseSearch(reverseResult, this.diphthongs, true))
-                    reverseResult = reverseResult.slice(0, 1)
-
-                tmpPush(reverseResult);
-                continue;
-            }
-
-            // Same as previous example but for dipthongs
-            leftoverPointer = leftoverCalc(probJointSyllable.length);
-
-            //
-            // === >  If an accent is found in any vowel in the dipthong, we treat it as a normal syllable and skip
-            //
-            if (!probJointSyllable || (probJointSyllable.includes("í")
-                || probJointSyllable.includes("ú")) && !this.diphthongsExceptions.includes(probJointSyllable)) {
-
-                if (this.hasAccent(currentLetter) || this.hasAccent(nextLetter)) {
-
-                    probJointSyllable = probJointSyllable.split("")[0];
-                    tmpPush(probJointSyllable);
-                    continue;
-
-                }
-
-                emptyPush(pointerCalc());
-                continue;
-            }
-
-            if (probJointSyllable.length === 3)
-                nextNextLetter = wordAsArray[index + 3] ?? "";
-
-            !leftoverPointer ? probJointSyllable += nextNextLetter : probJointSyllable;
-
-            if (!leftoverPointer) {
-
-                let lastEst = this.getEst(word.slice((pointerCalc() + probJointSyllable.length)));
-                let lastChars = word.slice((pointerCalc() + probJointSyllable.length));
-                let lastEstExt = this.getEstExt(word.slice((pointerCalc() + probJointSyllable.length)));
-
-                lastChars = lastChars.replaceAll(/a|e|i|o|u/g, "");
-                lastEstExt = lastEstExt.includes("V") ? lastEstExt.join("").split("V")[0] : lastEstExt.join("");
-
-                if (lastEst === "CVC" || this.isValidSyllablesEst([lastEstExt], [lastChars]))
-                    probJointSyllable = probJointSyllable.slice(0, probJointSyllable.length - 1)
-            }
-            tmpPush(probJointSyllable);
-
+            tmpPush(currentLetter);
+            syllables = [...rulesApply(syllables)];
         }
+
         console.timeEnd("miScript");
-        if (cap)
-            syllables[0] = this.capitalize(syllables[0]);
-
-        if (isupc)
-            syllables = syllables.map((s) => s.toUpperCase());
-
-        return postProcessing(syllables);
+        return rulesApply(syllables);
     }
 
 }
